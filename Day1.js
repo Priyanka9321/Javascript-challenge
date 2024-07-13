c5eed4b9d7077d3da6b22d863dccdee32178ff9b
Priyanka yadav <priyanyadav52004@gmail.com>
2024-07-13 16:34:23 +0530

    Day1

---
+++ Day1.js
@@ -0,0 +1,48 @@
+// DAY 1: VARIABLE AND DATA TYPES
+// Activity 1 : Variable and Decalaration
+// Task 1
+var number = 10;
+console.log(number);
+
+// Task 2
+let string = "Priyanka";
+console.log(name);
+
+// Activity 2 : Constant Decalaration
+// Task 3
+const booleaVal = true;
+console.log(booleaVal);
+
+// Activity 3 : Data Types
+// Task 4
+let age = 20; //number
+console.log(typeof(age));
+
+let name = "sneha"; //string
+console.log(typeof(name));
+
+let isLogin = true; //boolean
+console.log(typeof(isLogin));
+
+let Info = {  // object
+    firstName: "Priyanka",
+    lastName: "Yadav"
+}
+console.log(typeof(Info));
+
+let marks = [90, 89, 50, 70]; //array
+console.log(typeof(marks));
+
+// Activity 4 : Reassigning variables 
+// Task 5
+let initialValue = 10;
+console.log(initialValue);
+
+initialValue = 20;
+console.log(initialValue);
+
+// Activity 5 : Understanding const 
+// Task 6 
+const PI = 3.14;
+PI = 2.8;
+console.log(PI);
